edfbcdc5ca1c63ede143e80c137946866ababec5
bhattshrey24 <bhatt.shrey1224@gmail.com>
2022-06-15 11:12:10 +0530

    Comments Added

--- app/src/androidTest/java/com/example/practiceappforunittesting/data/local/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/example/practiceappforunittesting/data/local/ShoppingDaoTest.kt
@@ -15,39 +15,44 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
-@SmallTest
+@ExperimentalCoroutinesApi // this annotation should be used when you are using coroutines in test cases like we used "runBlockingTest" here. I guess its not necessary to add this notation but you should add it
+@RunWith(AndroidJUnit4::class) // Now since here we are usig android specific libraries like context etc therefore JUnit cannot run it directly on JVM like it did in "test" package , therefore we have to apply this annotation to tell JUnit to runt these tests in Emulator
+@SmallTest // There are 3 annotation for “androidTest” package, “@SmallTest” if you want to do Unit Testing , “@MediumTest” for Integerated Testing and “@LargeTest” annotation for UI testing. Since here we are doing Unit testing therefore I used this annotation
 class ShoppingDaoTest {
 
-    @get:Rule
-    var instantTaskExecutorRule = InstantTaskExecutorRule()
+    @get:Rule // This annotation tells JVM to apply the Rule for all the test cases , Like here we are telling JVM to Apply the rule that all test cases should run synchronously
+    var instantTaskExecutorRule = InstantTaskExecutorRule() // This as the name suggest tells JVM to run all the test cases synchronously
 
     private lateinit var database: ShoppingItemDatabase
     private lateinit var dao: ShoppingDao
 
-    @Before
+    @Before // This annotation tells JVM to execute this function everytime before executing a test case(Mtlb har test case execute hone se pehle yeh wala function chalega) , so here you can do initialization like we did here
     fun setup() {
+       // database=Room.databaseBuilder()// Usually we do this in our source code but in testing since we only need to test therefore we use a different approach as shown below
+
+        // In testing we use "inMemoryDatabaseBuilder()" instead of "databaseBuilder()" to make Db cause it will only make database in RAM which will get erased after each test case execution this is useful because we don't want to create multiple Databases in our persistent memory just for the sake of testing cause that will just waste memory
         database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
+            ApplicationProvider.getApplicationContext(), // This is how we get context
             ShoppingItemDatabase::class.java
-        ).allowMainThreadQueries().build()
-        dao = database.shoppingDao()
+        ).allowMainThreadQueries().build() //"allowMainThreadQueries" as the name suggest this function will allow  allow this Room database to be accessed by Main thread , now generally we access DB using background thread which runs asynchronously but we do not want async behaviour in test cases therefore we let main thread execute it
+
+        dao = database.shoppingDao() // just initializing the Dao with Database so that we can use the function present in it
     }
 
-    @After
+    @After// This annotation tells JVM to run this function after each test case is executed (Mtlb har test case execute hone ke baad yeh wala function chalega) , so here you can close connections like the way we closed the db
     fun teardown() {
         database.close()
     }
 
     @Test
-    fun insertShoppingItem() = runBlockingTest {
+    fun insertShoppingItem() = runBlockingTest { // runBlockingTest is a coroutine . This is optimised for test cases like it will skip the "delay" function cause we dont want our test case to delay ie. they should be fast
+
         val shoppingItem = ShoppingItem("name", 1, 1f, "url", id = 1)
         dao.insertShoppingItem(shoppingItem)
 
-        val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()
+        val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()  //Now here is a problem , this function returns a liveData and liveData is async and we don't want async behaviour in our testcases so the solution is this "getOrAwaitValue" function which is from the google class "LiveDataUtilAndroidTest" that we added.
 
-        Truth.assertThat(allShoppingItems).contains(shoppingItem)
+        Truth.assertThat(allShoppingItems).contains(shoppingItem)// this function will check if "allShoppingItems" list contains "shoppingItem" or not
     }
 
     @Test
@@ -66,6 +71,7 @@ class ShoppingDaoTest {
         val shoppingItem1 = ShoppingItem("name", 2, 10f, "url", id = 1)
         val shoppingItem2 = ShoppingItem("name", 4, 5.5f, "url", id = 2)
         val shoppingItem3 = ShoppingItem("name", 0, 100f, "url", id = 3)
+
         dao.insertShoppingItem(shoppingItem1)
         dao.insertShoppingItem(shoppingItem2)
         dao.insertShoppingItem(shoppingItem3)
